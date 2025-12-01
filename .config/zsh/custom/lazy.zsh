5f4a18d407fd7a685573a0add197ac0e5566e1f9
Zoe <Zoe.Wick@outlook.com>
2025-12-01 15:35:30 +0800

    fix(lazysdk): follow SDKMAN_DIR

--- .config/zsh/custom/lazy.zsh
+++ .config/zsh/custom/lazy.zsh
@@ -1,6 +1,5 @@
 sdk() {
     unfunction sdk
-    () { [[ -f $1 && -r $1 ]] && source $1; } \
-        $HOME/.sdkman/bin/sdkman-init.sh
+    source ${SDKMAN_DIR:-$HOME/.sdkman}/bin/sdkman-init.sh
     sdk $@
 }
